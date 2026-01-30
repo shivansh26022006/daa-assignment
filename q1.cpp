016c49dfdf0a7b804737ebe7402e38eaa7bafe09
shivansh26022006 <ssharma14_be24@thapar.edu>
2026-01-30 12:01:34 +0530

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -1,27 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int binarysearch(int arr[],int x,int n){
-	int low=0;
-	int high=n-1;
-	while(low<=high){
-		int mid=low+(high-low)/2;
-		if(arr[mid]==x)
-		  return mid;
-		if(arr[mid]<x)
-		  low=mid+1;
-		else
-		  high=mid-1;
-	}
-	return -1;
+int binarysearch(int arr[], int x, int n) {
+    int low = 0;
+    int high = n - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == x)
+            return mid;
+        else if (arr[mid] < x)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
 }
-int main(){
-	int n=10;
-	int arr[]={3,10,90,89,76,100,55.78};
-	int x=55;
-	int result=binarysearch(arr,x,n);
-	if(result=-1)
-	  cout<<"element is not present in array"<<endl;
-	else
-	  cout<<"element is present at index"<<result;
-	return 0;
+int main() {
+    int arr[] = {3, 10, 55, 76, 89, 90, 100}; // sorted array
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 55;
+    int result = binarysearch(arr, x, n);
+    if (result == -1)
+        cout << "Element is not present in array" << endl;
+    else
+        cout << "Element is present at index " << result << endl;
+    return 0;
 }
